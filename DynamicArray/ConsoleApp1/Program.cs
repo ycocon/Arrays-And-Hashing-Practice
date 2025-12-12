4952375ce55d7fb579f43ca23884f3012d50c89a
yco <andreidejesus06@gmail.com>
2025-12-12 13:20:36 +0800

    Initial commit

---
+++ DynamicArray/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿List<int> arr = new List<int>();
+
+Console.WriteLine(arr.Capacity);
+
+arr.Add(1);
+
+Console.WriteLine(arr.Capacity);
+
+arr.Add(2);
+
+Console.WriteLine(arr.Capacity);
+
+arr.Add(3);
+arr.Add(4);
+
+Console.WriteLine(arr.Capacity);
+
+arr.Add(5);
+
+Console.WriteLine(arr.Capacity);
