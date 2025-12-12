a5b7bdd8a8a57befa876a034a436fbdabb413188
yco <andreidejesus06@gmail.com>
2025-12-12 13:26:02 +0800

    Add .gitignore to exclude build and IDE files
    Added a .gitignore file to exclude build outputs, IDE-specific folders, user-specific files, logs, and NuGet packages from version control. This helps keep the repository clean and prevents unnecessary files from being tracked.

--- DynamicArray/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ DynamicArray/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4952375ce55d7fb579f43ca23884f3012d50c89a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
